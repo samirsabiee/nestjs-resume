d9fafb31f78739e686f70afe1cf0ef13657e862e
samir sabiee <samirsabiee@gmail.com>
2021-07-07 14:39:19 +0430

    add typeOrmModuleOption

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticleModule } from './article/article.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {mongodbConfig} from "./dbConfig/mongodb.config";
 
 @Module({
-  imports: [ArticleModule],
+  imports: [TypeOrmModule.forRoot(mongodbConfig), ArticleModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/article/entities/article.entity.ts
+++ src/article/entities/article.entity.ts
@@ -1,1 +1,19 @@
-export class Article {}
+import {Column, Entity, ObjectID, ObjectIdColumn} from "typeorm";
+
+@Entity('Article')
+export class Article {
+    @ObjectIdColumn()
+    _id: ObjectID
+
+    @Column()
+    title: string
+
+    @Column()
+    author: string
+
+    @Column()
+    cover: string
+
+    @Column()
+    content: string
+}
