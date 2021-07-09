fe44e0b99b92d928174e97d66f4dbd417e31b6d4
samir sabiee <samirsabiee@gmail.com>
2021-07-09 18:39:04 +0430

    add dto and validationPipe

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,13 @@
-export class CreateUserDto {}
+import {IsString, Matches, MaxLength, MinLength} from "class-validator";
+
+export class CreateUserDto {
+    @IsString()
+    @MinLength(8)
+    @MaxLength(30)
+    username: string
+
+    @IsString()
+    @MinLength(8)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)
+    password: string
+}
